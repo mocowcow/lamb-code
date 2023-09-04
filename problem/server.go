fd2df347b17497ace5896145dd975793c10eb883
ariduki <ariduki@gmail.com>
2023-09-04 22:32:01 +0800

    feat: get testcases

--- problem/server.go
+++ problem/server.go
@@ -14,9 +14,12 @@ var db *gorm.DB
 
 func RunServer() {
 	db, _ = utils.GetDB("problem")
+
 	r := gin.Default()
 	r.GET("/problems", getProblems)
 	r.GET("/problems/:id", getProblem)
+	r.GET("/problems/:id/testcases", getTestcases)
+
 	r.Run(config.PROBLEM_ADDR)
 }
 
@@ -34,6 +37,7 @@ func getProblems(ctx *gin.Context) {
 		problems,
 	)
 }
+
 func getProblem(ctx *gin.Context) {
 	id := ctx.Param("id")
 	var problem problem
@@ -41,5 +45,22 @@ func getProblem(ctx *gin.Context) {
 	ctx.JSON(
 		http.StatusOK,
 		problem,
+	)
+}
+
+type testcase struct {
+	Id        int
+	ProblemId int
+	Input     string
+	Output    string
+}
+
+func getTestcases(ctx *gin.Context) {
+	id := ctx.Param("id")
+	var testcases []testcase
+	db.Where("problem_id = ?", id).Find(&testcases)
+	ctx.JSON(
+		http.StatusOK,
+		testcases,
 	)
 }
