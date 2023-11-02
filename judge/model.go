be0246d592015c11d21550733dffa7dcb51515bd
ariduki <ariduki@gmail.com>
2023-11-02 22:48:46 +0800

    add code template for golang

--- judge/model.go
+++ judge/model.go
@@ -9,3 +9,28 @@ type testcase struct {
 	Input  string
 	Output string
 }
+
+var CODE_TEMPLATE map[string]string
+
+func init() {
+	initGolangTemplate()
+}
+
+func initGolangTemplate() {
+	CODE_TEMPLATE["go"] = `
+	package main
+	
+	import (
+		"fmt"
+	)
+	
+	func main() {
+		//// receive inputs from stdin
+		// var a, b int
+	
+		//// output the answer
+		// ans := a + b
+		// fmt.Printf("ans=%d", ans)
+	}
+	`
+}
