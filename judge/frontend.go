d4c6dbac5aa0d07f5b904f8c9285b7165b5440c7
ariduki <ariduki@gmail.com>
2023-11-02 22:52:10 +0800

    add template api

--- judge/frontend.go
+++ judge/frontend.go
@@ -7,8 +7,15 @@ import (
 func frontendRouting(r *gin.Engine) {
 	r.LoadHTMLGlob("./judge/*.html")
 	r.GET("/index/:id", getIndex)
+	r.GET("/template/:lang", getTemplate)
 }
 
 func getIndex(ctx *gin.Context) {
 	ctx.HTML(200, "index.html", nil)
+}
+
+func getTemplate(ctx *gin.Context) {
+	lang := ctx.Param("lang")
+	template := CODE_TEMPLATE[lang]
+	ctx.String(200, template)
 }
